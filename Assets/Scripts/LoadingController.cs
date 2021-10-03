b669f8005ccf111d9bfd3f4bf1b344afebc18fe6
Gavin Bagus <gavinbagus123@gmail.com>
2021-10-03 21:07:53 +0700

    Initial commit, local save sudah bekerja

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour
+{
+    [SerializeField] private Button _localButton;
+    [SerializeField] private Button _cloudButton;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UserDataManager.Load();
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UserDataManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+// mengatur userprogress data untuk disimpan atau diload
+public static class UserDataManager
+{
+    private const string PROGRESS_KEY = "Progress";
+    public static UserProgressData Progress;
+
+    public static void Load()
+    {
+        // cek apakah ada data yang tersimpan
+        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
+        {
+            // JIKA TIDAK ADA BUAT BARU dan upload ke cloud
+            Progress = new UserProgressData();
+            Save(true);
+        }
+        else
+        {
+            // jika ada, overwrite dengan progress sebelumnya
+            string json = PlayerPrefs.GetString(PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData>(json);
+        }
+    }
+
+    public static void Save(bool uploadToCloud = false)
+    {
+        string json = JsonUtility.ToJson(Progress);
+        PlayerPrefs.SetString(PROGRESS_KEY, json);
+    }
+
+    public static bool HasResources(int index)
+    {
+        return index + 1 <= Progress.ResourcesLevels.Count;
+    }
+}
